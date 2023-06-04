52856ccd77deadf47bb905a4574f46ff81c35a65
Rabbi-Sheikh <96rabby@gmail.com>
2023-06-04 18:12:39 +0600

    Class 2 end

--- resources/views/backend/auth/login.blade.php
+++ resources/views/backend/auth/login.blade.php
@@ -4,13 +4,19 @@
 @section('content')
     {!! Form::open([]) !!}
     {!! Form::label('email','Email') !!}
-    {!! Form::email('email',null,['class'=>'form-control form-control-sm','placeholder'=>'Enter your email']) !!}
+    {!! Form::email('email',null,['class'=> $errors->has('email') ? 'is-invalid form-control form-control-sm' : 'form-control form-control-sm','placeholder'=>'Enter your email']) !!}
+    @error('email')
+        <p class="text-danger">{{$message}}</p>
+    @enderror
     {!! Form::label('password','Password',['class' => 'mt-2']) !!}
-    {!! Form::password('password',['class'=>'form-control form-control-sm','placeholder'=>'Enter your password']) !!}
+    {!! Form::password('password',['class'=>$errors->has('password') ? 'is-invalid form-control form-control-sm' : 'form-control form-control-sm','placeholder'=>'Enter your password']) !!}
+    @error('password')
+    <p class="text-danger">{{$message}}</p>
+    @enderror
     <div class="d-grid">
         {!! Form::button('Login',['type' => 'submit','class'=>'btn btn-sm btn-info mt-3']) !!}
     </div>
     {!! Form::close() !!}
-    <p class="mt-2" >Forgot Password? <a href="{{route('password.request')}}">Forgot Password</a></p>
+    <p class="mt-2" >Forgot Password? <a href="{{route('password.request')}}">Reset Here</a></p>
     <p>Not registered? <a href="{{route('register')}}">Register Here</a></p>
 @endsection
--- resources/views/backend/auth/register.blade.php
+++ resources/views/backend/auth/register.blade.php
@@ -4,17 +4,28 @@
 @section('content')
     {!! Form::open(['method' => 'post','route'=>'register']) !!}
     {!! Form::label('name','Name') !!}
-    {!! Form::text('name',null,['class'=>'form-control form-control-sm','placeholder'=>'Enter your name']) !!}
+    {!! Form::text('name',null,['class'=>$errors->has('name') ? 'is-invalid form-control form-control-sm' : 'form-control form-control-sm','placeholder'=>'Enter your name']) !!}
+    @error('name')
+    <p class="text-danger">{{$message}}</p>
+    @enderror
     {!! Form::label('email','Email') !!}
-    {!! Form::email('email',null,['class'=>'form-control form-control-sm','placeholder'=>'Enter your email']) !!}
+    {!! Form::email('email',null,['class'=>$errors->has('email') ? 'is-invalid form-control form-control-sm' : 'form-control form-control-sm','placeholder'=>'Enter your email']) !!}
+    @error('email')
+    <p class="text-danger">{{$message}}</p>
+    @enderror
     {!! Form::label('password','Password',['class' => 'mt-2']) !!}
-    {!! Form::password('password',['class'=>'form-control form-control-sm','placeholder'=>'Enter your password']) !!}
+    {!! Form::password('password',['class'=>$errors->has('password') ? 'is-invalid form-control form-control-sm' : 'form-control form-control-sm','placeholder'=>'Enter your password']) !!}
+    @error('password')
+    <p class="text-danger">{{$message}}</p>
+    @enderror
     {!! Form::label('password_confirmation','Password Confirmation',['class' => 'mt-2']) !!}
-    {!! Form::password('password_confirmation',['class'=>'form-control form-control-sm','placeholder'=>'Enter your confirm password']) !!}
+    {!! Form::password('password_confirmation',['class'=>$errors->has('password_confirmation') ? 'is-invalid form-control form-control-sm' : 'form-control form-control-sm','placeholder'=>'Enter your confirm password']) !!}
+    @error('password_confirmation')
+    <p class="text-danger">{{$message}}</p>
+    @enderror
     <div class="d-grid">
-        {!! Form::button('Login',['type' => 'submit','class'=>'btn btn-sm btn-info mt-3']) !!}
+        {!! Form::button('Register',['type' => 'submit','class'=>'btn btn-sm btn-info mt-3']) !!}
     </div>
     {!! Form::close() !!}
-    <p class="mt-2" >Forgot Password? <a href="{{route('password.request')}}">Forgot Password</a></p>
-    <p>Not registered? <a href="{{route('register')}}">Register Here</a></p>
+    <p class="mt-2" >Already registered? <a href="{{route('login')}}">Login Here</a></p>
 @endsection
